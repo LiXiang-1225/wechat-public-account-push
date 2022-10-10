98d12bb77cf95d49b5130009fa11c4166869a9c8
LiXiang <57379597+LiXiang-1225@users.noreply.github.com>
2022-10-10 15:45:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oFOzH6JIMrzotQa6rwTX0MnQkbd0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Jjuhy3nAHMfOzon9h4yneDNLmEh1gX4jhNzwqh6woEw',
+      useTemplateId: 'qJ_csnStZXPwoo-s0Z_ISlapZfugS2t7whmEIP104_M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-15',
       openUrl: 'www.baidu.com',
